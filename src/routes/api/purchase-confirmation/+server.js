8922ab2446ae900ccd4e99e67376178ddb39cd0e
G. Troy Johnson <github@weave.sh>
2025-04-24 00:09:14 -0700

    variables do not escape try, move all dependant code inside block

--- src/routes/api/purchase-confirmation/+server.js
+++ src/routes/api/purchase-confirmation/+server.js
@@ -17,41 +17,42 @@ export async function POST({request}) {
         const stripeEvent = stripe.webhooks.constructEvent(body, signature, STRIPE_WEBHOOK_SECRET);
         const customerEmail = stripeEvent.data.object.customer_details.email;
         const customerName = stripeEvent.data.object.customer_details.name;
+        const response = await fetch(PDF_GUIDE_URL);
+        const pdfBuffer = await response.arrayBuffer();
+        const base64Pdf = Buffer.from(pdfBuffer).toString('base64');
+
+
+
+        const message = {
+            to: customerEmail,
+            from: "troy@weave.sh",
+            subject: "Your Purchase Confirmation",
+            html: `
+            <h1>Thank you for your purchase!</h1>
+            <p>Dear ${customerName},</p>
+            <p>Thank you for your recent purchase. We appreciate your business!</p>
+            <p>If you have any questions or need assistance, feel free to reach out to us.</p>
+            <p>Best regards,</p>
+            <p>The Weave Team</p>
+            `,
+            attachments: [
+                {
+                    content: base64Pdf,
+                    filename: "Digial Ebook - Spain relocation.pdf",
+                    type: "application/pdf",
+                    disposition: "attachment",
+                }
+            ]
+        }
+
+        await sgMail.send(message);
+        //console.log('Received request body:', requestBody);
+
+        return json({ response: "Email sent" });
     } catch (err) {
         console.error("Error verifying Stripe webhook signature:", err);
         return json({ error: "Invalid signature" }, { status: 400 });
     }
 
-    const response = await fetch(PDF_GUIDE_URL);
-    const pdfBuffer = await response.arrayBuffer();
-    const base64Pdf = Buffer.from(pdfBuffer).toString('base64');
-
-
-
-    const message = {
-        to: customerEmail,
-        from: "troy@weave.sh",
-        subject: "Your Purchase Confirmation",
-        html: `
-        <h1>Thank you for your purchase!</h1>
-        <p>Dear ${customerName},</p>
-        <p>Thank you for your recent purchase. We appreciate your business!</p>
-        <p>If you have any questions or need assistance, feel free to reach out to us.</p>
-        <p>Best regards,</p>
-        <p>The Weave Team</p>
-        `,
-        attachments: [
-            {
-                content: base64Pdf,
-                filename: "Digial Ebook - Spain relocation.pdf",
-                type: "application/pdf",
-                disposition: "attachment",
-            }
-        ]
-    }
-
-    await sgMail.send(message);
-    //console.log('Received request body:', requestBody);
-
-    return json({ response: "Email sent" });
+    
 }
