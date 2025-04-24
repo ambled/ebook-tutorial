bc77fb6845d8a5d5df02355d1f88dbd5e3110a44
G. Troy Johnson <github@weave.sh>
2025-04-23 23:59:34 -0700

    payload protection

--- src/routes/api/purchase-confirmation/+server.js
+++ src/routes/api/purchase-confirmation/+server.js
@@ -1,20 +1,32 @@
 import {json} from "@sveltejs/kit";
 import sgMail from "@sendgrid/mail";
-import { SENDGRID_API_KEY } from "$env/static/private";
+import { SENDGRID_API_KEY, STRIPE_WEBHOOK_SECRET, STRIPE_API_KEY } from "$env/static/private";
+import Stripe from "stripe";
 
 sgMail.setApiKey(SENDGRID_API_KEY);
+const stripe = new Stripe(STRIPE_API_KEY);
 
 const PDF_GUIDE_URL = "https://narrify-public.s3.eu-central-1.amazonaws.com/sample.pdf";
 
 export async function POST({request}) {
-    const requestBody = await request.json();
+
+    const body = await request.text();
+    const signature = request.headers.get("stripe-signature");
+
+    try {
+        const stripeEvent = stripe.webhooks.constructEvent(body, signature, STRIPE_WEBHOOK_SECRET);
+        const customerEmail = stripeEvent.data.object.customer_details.email;
+        const customerName = stripeEvent.data.object.customer_details.name;
+    } catch (err) {
+        console.error("Error verifying Stripe webhook signature:", err);
+        return json({ error: "Invalid signature" }, { status: 400 });
+    }
 
     const response = await fetch(PDF_GUIDE_URL);
     const pdfBuffer = await response.arrayBuffer();
     const base64Pdf = Buffer.from(pdfBuffer).toString('base64');
 
-    const customerEmail = requestBody.data.object.customer_details.email;
-    const customerName = requestBody.data.object.customer_details.name;
+
 
     const message = {
         to: customerEmail,
